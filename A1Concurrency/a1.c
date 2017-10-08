7dc74b5a1b9a45379d309e0f2e220002ac19ce53
Brian Wiltse <wiltseb@oregonstate.edu>
2017-10-08 01:08:12 -0700

    working with twister, needs rdrand

--- A1Concurrency/a1.c
+++ A1Concurrency/a1.c
@@ -1,25 +1,41 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
 
 #include "mt.h"
+
+#define MAX_SIZE 32
 
 /*
  * This is an item that goes in the buffer
  */
 struct item {
-  int number; // randomly generated number
+  unsigned long id; // randomly generated number
   int wait_time; // randomly generated number of seconds (between 2 and 9)
 };
 
-struct consumer {
-  int id;
-  int number; // takes on the value of item's number
+struct buffer {
+  struct item items[MAX_SIZE];
+  int size;
 };
 
-struct producer {
-  int id;
-  int wait_time; // between 3 and 7 seconds
+/*
+ * Initialize shared buffer
+ */
+struct buffer buffer = {
+  .size = 0
 };
+
+/* Initialize semaphores. 
+ * Consumer will block when sem_full is 0 (nothing to consume)
+ * Producer will block when sem_empty is 0 (buffer is full)
+ * mutex will allow only one producer or consumer access to the buffer
+ */
+sem_t sem_full;
+sem_t sem_empty;
+pthread_mutex_t mutex;
 
 /*
  * Generates a random number with rdrand x86 asm, if supported.
@@ -28,12 +44,12 @@ struct producer {
  * This code was adapted from Kevin McGrath's class website at:
  * http://web.engr.oregonstate.edu/cgi-bin/cgiwrap/dmcgrath/classes/17F/cs444/index.cgi?examples=1
  */
-int gen_rand_num() {
+unsigned long gen_rand_num() {
   unsigned int eax;
   unsigned int ebx;
   unsigned int ecx;
   unsigned int edx;
-  unsigned int random;
+  unsigned long random;
   char vendor[13];
   
   eax = 0x01;
@@ -45,29 +61,86 @@ int gen_rand_num() {
                        );
   
   if(ecx & 0x40000000){
-    printf("I support rdrand\n");
+    // printf("I support rdrand\n");
     // TODO: use rdrand
   } else{
-    printf("I do not support rdrand\n");
+    // printf("I do not support rdrand\n");
     random = genrand_int32();
   }
 
   if (random < 0 ) {
     random = random * -1;
   }
-     
   return random;
 }
 
-int main() {
+void print_buffer() {
+  printf("buffer size: %d\n", buffer.size);
+  for(int i = 0; i < buffer.size; i++) {
+    printf("%lu | ", buffer.items[i]);  
+  }
+  printf("\n");
+}
+
+
+void produce(int num_items) {
+  int p_wait_time;
+  struct item item;
+  for(int i = 0; i < num_items; i++) {
+    p_wait_time = (gen_rand_num() % 4) + 3;
+    item.id = gen_rand_num();
+    item.wait_time = (gen_rand_num() % 7) + 2;
+    
+    sleep(p_wait_time);
+    sem_wait(&sem_empty);
+    pthread_mutex_lock(&mutex);
+    buffer.items[buffer.size++] = item; 
+    printf("Producer waited %d seconds, then placed item %lu in buffer\n",
+             p_wait_time, item.id);
+    print_buffer();
+    pthread_mutex_unlock(&mutex);
+    sem_post(&sem_full);
+  }
+   
+}
+
+void *consume(void *tid) {
+  int cid = (int)tid;
+  struct item curr_item;
+  sem_wait(&sem_full);
+  pthread_mutex_lock(&mutex);
+    curr_item = buffer.items[--buffer.size];
+  pthread_mutex_unlock(&mutex);
+  sem_post(&sem_empty);
+  sleep(curr_item.wait_time);
+  printf("Consumer %d waited %d seconds, then consumed item %lu\n", 
+          cid, curr_item.wait_time, curr_item.id);
+     
+}
+
+int main(int argc, char* argv[]) {
+  pthread_mutex_init(&mutex, NULL);
+  sem_init(&sem_full, 0, 0);  
+  sem_init(&sem_empty, 0, MAX_SIZE);
   unsigned long init[4] = {0x123, 0x234, 0x345, 0x456};
   unsigned long length = 4;
-  init_by_array(init, length);  
+  int num_items = atoi(argv[1]); 
+  init_by_array(init, length); 
 
-  unsigned int x = 0;
-  printf("%d\n", x);
-  x = gen_rand_num();
-  printf("%d\n", x);
+  // create an array of threads for consumers
+  pthread_t* consumers = malloc(sizeof(pthread_t) * num_items);;
+ 
+  // create consumers
+  for (int i = 0; i < num_items; i++) {
+    pthread_create(&(consumers[i]), NULL, consume, (void*)i); 
+  }
+
+  // create producer
+  produce(num_items);
+   
+  for(int i = 0; i < num_items; i++) {
+    pthread_join(consumers[i], NULL);
+  }
 
   return 0;  
 }
